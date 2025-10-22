6e63344e208aae518027dfb405311ec20831096e
Mikhail Makeev <mihail.makeev@gmail.com>
2025-10-22 08:13:40 +0300

    feat(copilot): add copilot.lua plugin with custom options

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,39 @@
+return {
+  recommended = true,
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = "<Tab>",
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+
+  -- copilot-language-server
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- copilot.lua only works with its own copilot lsp server
+        copilot = { enabled = false },
+      },
+    },
+  },
+}
+
