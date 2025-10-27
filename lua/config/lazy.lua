dd1d7209a3b9c64679885a85ebbe99da2e727102
Mikhail Makeev <mihail.makeev@gmail.com>
2025-10-27 06:38:00 +0200

    chore(plugins): update plugin lockfile for latest versions

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -39,7 +39,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "github_light" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
