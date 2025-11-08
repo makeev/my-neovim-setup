e836cf1d20318f074f6652992d610875558b6aaa
Mikhail Makeev <mihail.makeev@gmail.com>
2025-11-08 19:52:40 +0200

    feat(copilot): add smart tab to accept suggestion or insert tab

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -13,7 +13,7 @@ return {
         debounce = 150, -- задержка для снижения нагрузки при печати
         hide_during_completion = false, -- не прятать copilot при показе blink.cmp
         keymap = {
-          accept = "<Tab>",
+          accept = false, -- отключаем стандартный биндинг
           accept_word = false,
           accept_line = false,
           next = "<M-]>",
@@ -27,6 +27,24 @@ return {
         help = true,
       },
     },
+    config = function(_, opts)
+      require("copilot").setup(opts)
+
+      -- Умный Tab: принимает copilot если есть предложение, иначе вставляет отступ
+      vim.keymap.set("i", "<Tab>", function()
+        local suggestion = require("copilot.suggestion")
+        if suggestion.is_visible() then
+          suggestion.accept()
+        else
+          -- Вставляем Tab или пробелы в зависимости от expandtab
+          if vim.o.expandtab then
+            return string.rep(" ", vim.o.shiftwidth)
+          else
+            return "\t"
+          end
+        end
+      end, { expr = true, silent = true, desc = "Accept Copilot or insert tab" })
+    end,
   },
 
   -- copilot-language-server
